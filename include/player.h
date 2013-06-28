d6e52f10f6d66f544213cd688416979488aca0f2
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-06-28 00:14:38 -0300

    Adding a new interface to separate player's charachters

---
+++ include/player.h
@@ -0,0 +1,25 @@
+/***************************************************/
+/**  MAC 0211  -  Laboratório de Programação I    **/
+/**  IME-USP   -  Primeiro  Semestre  de    2013  **/
+/**  Turma 45  -  Kelly Rosa Bragheto             **/
+/**                                               **/
+/**  Quarto    Exercício-Programa                 **/
+/**  Arquivo:  player.h                           **/
+/**                                               **/
+/**  Karina Suemi Awoki                  7572102  **/
+/**  Renato Cordeiro Ferreira            7990933  **/
+/**  Ruan de Menezes Costa               7990929  **/
+/**                                               **/
+/**  Em caso de eventuais problemas, acesse:      **/
+/**  git@github.com:renatocf/MAC0211-EP4.git      **/
+/***************************************************/
+
+#ifndef H_PLAYER_DEFINED
+#define H_PLAYER_DEFINED
+
+typedef struct player Player;
+struct player {
+    int lifes;
+};
+
+#endif
